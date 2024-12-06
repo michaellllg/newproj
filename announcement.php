254d8bf852ce76e920155429feca5e5b3430e477
aalorro <athina.alorro@gmail.com>
2024-12-06 12:02:55 +0800

    :>

--- announcement.php
+++ announcement.php
@@ -100,7 +100,6 @@ background-color: #344687 !important; /* Darker shade for hover effect */
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 =======
-<?php include('nav.php'); ?>
 
 
 <div class="main-container">
