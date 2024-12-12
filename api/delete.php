b22f917ce6c0e0b6af26ff48618d67bae429ed90
michaellllg <naciom007@gmail.com>
2024-12-12 20:01:25 +0800

    l

--- api/delete.php
+++ api/delete.php
@@ -1,14 +1,7 @@
 <?php
 // Include database connection
-include 'login.php';
+include 'connection.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Get the ID from the query string
 if (isset($_GET['id'])) {
