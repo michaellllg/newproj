3d2eadacf20d25e29603041eaa48cb57512f2736
michaellllg <naciom007@gmail.com>
2024-12-15 06:24:02 +0800

    asdfg

--- member.php
+++ member.php
@@ -213,26 +213,6 @@ $conn->close();
 </div>
 
 
-<!-- Confirmation Modal -->
-<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmationModalLabel" style="background-color: #0F3E84;">Confirm Add Member</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to add this member?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                <button type="button" class="btn btn-primary" id="confirmAddMemberBtn" style="background-color: #0F3E84;" >Yes, Add Member</button>
-            </div>
-        </div>
-    </div>
-</div>
 
   
   <div class="form-popup"></div>
